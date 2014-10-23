6b66e0556c4ab7500009b34ad7d0be476a35cec2
aromatt <amatteson3@gmail.com>
2014-10-23 17:26:11 -0400

    Kill entire process group upon exit; Make pid and log files configurable
    Also unbuffer stdout and stderr; mkdir -p log and pid files

--- lib/simple-daemon.rb
+++ lib/simple-daemon.rb
@@ -1,27 +1,29 @@
 require 'simple-daemon/version'
 require 'fileutils'
 
-module SimpleDaemon
+module Simple-Daemon
   class Base
 
-    def self.classname      
-      underscore(name.split("::").last)
+    attr_reader :pid_file, :log_file
+    def initialize(opts = {})
+      @pid_file = opts[:pid_file] || "/var/run/#{classname}.pid"
+      @log_file = opts[:log_file] || "/var/log/#{classname}.log"
     end
 
-    def self.pid_fn
-      File.join(SimpleDaemon::WORKING_DIRECTORY, "#{classname}.pid")
-    end
-
-    def self.daemonize
+    def daemonize
       Controller.daemonize(self)
+    end
+
+    def classname
+      underscore(self.class.name.split("::").last)
     end
 
     private
 
-    def self.underscore(camel_cased_word)
+    def underscore(camel_cased_word)
       camel_cased_word.to_s.
-      gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-      gsub(/([a-z\d])([A-Z])/,'\1_\2').
+      gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
+      gsub(/([a-z\d])([A-Z])/, '\1_\2').
       tr("-", "_").
       downcase
     end
@@ -29,11 +31,15 @@ module SimpleDaemon
 
   module PidFile
     def self.store(daemon, pid)
-      File.open(daemon.pid_fn, 'w') {|f| f << pid}
+      dir = File.dirname(daemon.pid_file)
+      if not File.directory?(dir)
+        FileUtils.mkdir_p(dir)
+      end
+      File.open(daemon.pid_file, 'w') { |f| f << pid }
     end
 
     def self.recall(daemon)
-      IO.read(daemon.pid_fn).to_i rescue nil
+      IO.read(daemon.pid_file).to_i rescue nil
     end
   end
 
@@ -57,31 +63,46 @@ module SimpleDaemon
       fork do
         Process.setsid
         exit if fork
-        if File.file?(daemon.pid_fn)
-          puts "Pid file #{daemon.pid_fn} already exists.  Not starting."
+        if File.file?(daemon.pid_file)
+          puts "Pid file #{daemon.pid_file} already exists.  Not starting."
           exit 1
         end
         PidFile.store(daemon, Process.pid)
-        Dir.chdir SimpleDaemon::WORKING_DIRECTORY
+        dir = File.dirname daemon.log_file
+        FileUtils.mkdir_p dir if not File.directory? dir
+        Dir.chdir dir
         File.umask 0000
-        log = File.new("#{daemon.classname}.log", "a")
+        log = File.new("#{daemon.log_file}", "a")
         STDIN.reopen "/dev/null"
         STDOUT.reopen log
+        STDOUT.sync = true
         STDERR.reopen STDOUT
-        trap("TERM") {daemon.stop; exit}
+        STDERR.sync = true
+        trap("TERM") { daemon.stop; exit }
         daemon.start
       end
       puts "Daemon started."
     end
 
     def self.stop(daemon)
-      if !File.file?(daemon.pid_fn)
+      if !File.file?(daemon.pid_file)
         puts "Pid file not found. Is the daemon started?"
         exit
       end
+
+      # Get the pid from the pid file and remove the pid file
       pid = PidFile.recall(daemon)
-      FileUtils.rm(daemon.pid_fn)
-      pid && Process.kill("TERM", pid)
+      FileUtils.rm(daemon.pid_file)
+
+      # Kill all of the processes in the group
+      pgid = Process.getpgid(pid)
+      if pgid
+        Process.kill("-TERM", pgid)
+        puts "Daemon stopped."
+      else
+        puts "Cannot find process group id for pid #{pid}. Killing #{pid} only."
+        Process.kill("TERM", pid)
+      end
     rescue Errno::ESRCH
       puts "Pid file found, but process was not running. The daemon may have died."
     end
